2319a53687b7e9667489ebd9b1eec0d2b5eb6273
Varghese Chacko <varghesec@gmail.com>
2018-10-27 22:03:53 +0530

    Fix middleware

--- site_basics/middleware.py
+++ site_basics/middleware.py
@@ -1,12 +1,26 @@
 from django.conf import settings
-from django.core.urlresolvers import is_valid_path
+from django.urls import is_valid_path
 from django.http import HttpResponsePermanentRedirect
 from django.middleware.locale import LocaleMiddleware
 from django.utils import translation
 from django.utils.cache import patch_vary_headers
 
 class UpdatedLocaleMiddleware(LocaleMiddleware):
-    
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
     def process_response(self, request, response):
         language = translation.get_language()
         if (response.status_code == 404 and
